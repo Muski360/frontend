9fe6fd8f1454c0a1e0fd985201bb192a438b9dc8
Muski360 <mudb360@gmail.com>
2026-04-07 17:06:49 -0300

    07/04 - Projeto MVC - lista de tarefas

---
+++ POO_JavaScript/projeto_mvc/js/model/TarefaModel.js
@@ -0,0 +1,48 @@
+// base da estrutura do aplicativo
+// é a classe responsável pelos dados 
+
+// sera utilizada em outras partes do código
+//classe exportavel
+export class TarefaModel {
+  //construtor da classe de tarefas
+  constructor() {
+    this.tarefas = [];
+    this.currentId = 1;
+  }
+
+  addTarefa(titulo) {
+    const newTarefa = {
+      id: this.currentId++,
+      titulo: titulo,
+      completed: false,
+    }; //coleção
+    this.tarefas.push(newTarefa); //adicionando a tarefa ao vetor
+  }
+
+  getTarefas() {
+    return this.tarefas;
+  } //busca tdas as tarefas do vetor
+
+  atualizarTarefa(id) {
+    const tarefa = this.tarefas.find((tarefa) => tarefa.id === id);
+    if (tarefa) {
+      //se tarefa for encontrada
+      tarefa.completed = !this.tarefas.completed; //inverte o valor da booleana
+    }
+  }
+
+  removeTarefa(id) {
+    this.tarefas = this.tarefas.filter((tarefa) => tarefa.id !== id);
+    // reescrever a tarefa
+  }
+}
+
+// o que foi feito:
+ // criamos a lista de tarefas
+ // gera um id automatico para a tarefa
+ // adiciona tarefas
+ // busca as tarefas do vetor
+ // altera a tarefa
+ // remove a tarefa
+
+ 
---
+++ POO_JavaScript/projeto_mvc/js/view/TarefaView.js
@@ -0,0 +1,74 @@
+// classe para organização da interface da aplicação
+
+export class TarefaView {
+  constructor() {
+    this.tarefaInput = document.getElementById("tarefaInput"); //entrada da nova tarefa
+    this.addTarefaBtn = document.getElementById("addTarefaBtn");
+    this.listaTarefas = document.getElementById("ListaTarefa");
+    this.mensagem = document.getElementById("mensagem");
+  }
+  getTarefaInputValue() {
+    return this.tarefaInput.value.trim();
+    //pega o valor do input
+  }
+
+  clearInput() {
+    this.tarefaInput.value = "";
+    //limpa o campo do input
+  }
+
+  showMensagem(texto) {
+    this.mensagem.textContent = texto;
+  }
+
+  clearMensagem() {
+    this.mensagem.textContent = "";
+  }
+
+  // método para renderizar a tarefa na tela
+
+  renderTarefa(tarefas, atualizar, remover) {
+    //limpar a lista de tarefas
+    this.listaTarefas.innerHTML = "";
+
+    tarefas.forEach((tarefa) => {
+      const li = document.createElement("li");
+
+      if (tarefa.completed) {
+        li.classList.add("completed"); //risca a tarefa se estivercompleta
+      }
+      const span = document.createElement("span");
+      span.textContent = tarefa.titulo;
+
+      const action = document.createElement("div");
+      action.classList.add("action");
+
+      // Criar um botão para adicionar a tarefa (li)
+      const atualizaBtn = document.createElement("button");
+      atualizaBtn.textContent = tarefa.completed ? "Desfazer" : "Concluir";
+      atualizaBtn.addEventListener("click", () => atualizar(tarefa.id));
+
+      // criar o botão para remover
+      const removerBtn = document.createElement("button");
+      removerBtn.textContext = "Remover";
+      removerBtn.addEventListener("click", () => remover(tarefa.id));
+
+      //adicionando os BTN a lista de tarefas para cada tarefa
+      action.appendChild(atualizaBtn);
+      action.appendChild(removerBtn);
+
+      //adicionando elementos no LI
+      li.appendChild(span);
+      li.appendChild(action);
+
+      //adicionando a lista a UL
+      this.listaTarefas.appendChild(li);
+    });
+  }
+}
+
+// Pega elementos do HTML
+// Le um valor do Input
+// Limpa o Input
+// Mostra mensagem 
+// renderiza a lista de tarefas
